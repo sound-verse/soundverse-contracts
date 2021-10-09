5f34074410a8f1fdff958664302a9b4366799872
Camilo Ariza <andrescamilo.ariza@otto.de>
2021-10-09 23:14:16 +0200

    modify erc721 contracr

--- contracts/SoundVerseNFT.sol
+++ contracts/SoundVerseNFT.sol
@@ -1,35 +1,55 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract SoundVerseNFT is ERC721URIStorage {
-
+contract SoundVerseNFT is ERC721URIStorage, Ownable {
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    address public minterAddress;
 
-    constructor(address minter) ERC721("SoundVerse Music Tokens", "SVMT") {
-        minterAddress = minter;
+    //Constants and variables
+    uint256 public constant PRICE = 0.1 ether;
+    uint256 public constant START_AT = 1;
+
+    Counters.Counter private _tokenIds;
+
+    uint256 internal fee;
+
+    //Events
+    event NewMintEvent(uint256 indexed id);
+
+    constructor() ERC721("SoundVerse", "SVMT") {
+       fee = 0.1 * 10 ** 18;
     }
 
-    function createUnpublishedItem(string memory tokenURI) public returns (uint) {
-        _tokenIds.increment();
+    // Minting functions
+    function createUnpublishedItem(string memory tokenURI) public payable {
+        require(msg.value >= PRICE, "Value below price");
+    
+        address unpublishedOwner = _msgSender();
         uint256 newItemId = _tokenIds.current();
 
-        _safeMint(msg.sender, newItemId);
+        _mintItem(unpublishedOwner, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        setApprovalForAll(minterAddress, true);
-        return newItemId;
+
+        setApprovalForAll(unpublishedOwner, true);
     }
 
-    function burnUnpublishedItem(uint256 tokenId) public {
+    function _mintItem(address _to, uint256 _tokenId) private {
+
+        _tokenIds.increment();
+        _safeMint(_to, _tokenId);
+
+        emit NewMintEvent(_tokenId);
+    }
+
+    //Burning function
+    function burnUnpublishedItem(uint256 tokenId) public onlyOwner {
         _burn(tokenId);
     }
-
-    
 
 }
