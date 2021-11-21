25b24e49865148469623e43b2e7d028a73418b0a
Camilo Ariza <andrescamilo.ariza@otto.de>
2021-11-21 17:53:11 +0100

    implement own set uri method

--- contracts/SoundVerseERC1155.sol
+++ contracts/SoundVerseERC1155.sol
@@ -9,21 +9,47 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SoundVerseERC1155 is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pausable, Ownable {
-
+contract SoundVerseERC1155 is
+    Context,
+    AccessControlEnumerable,
+    ERC1155Burnable,
+    ERC1155Pausable,
+    Ownable
+{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    uint256 public constant MAX_SUPPLY = 500;
+
+    mapping(uint256 => string) private _uris;
 
     /**
      * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE`, and `PAUSER_ROLE` to the account that
      * deploys the contract.
      */
-    constructor(string memory uri) ERC1155(uri) {
+    constructor() ERC1155("") {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 
         _setupRole(MINTER_ROLE, _msgSender());
         _setupRole(PAUSER_ROLE, _msgSender());
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                              
+    }
+
+    /**
+     * @dev Returns the actual `baseURI` for token type `id`.
+     *
+     */
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return (_uris[tokenId]);
+    }
+
+    /**
+     * @dev Sets token URI for token type `id`.
+     *
+     */
+    function setTokenUri(uint256 tokenId, string memory _uri) public onlyOwner {
+        require(bytes(_uris[tokenId]).length == 0, "Cannot set uri twice");
+        _uris[tokenId] = _uri;
+    }
 
     /**
      * @dev Creates `amount` new tokens for `to`, of token type `id`.
@@ -41,7 +67,11 @@ contract SoundVerseERC1155 is Context, AccessControlEnumerable, ERC1155Burnable,
         bytes memory data
     ) public virtual {
         _setupRole(MINTER_ROLE, msg.sender);
-        require(hasRole(MINTER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have minter role to mint");
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERC1155PresetMinterPauser: must have minter role to mint"
+        );
+        require(amount <= MAX_SUPPLY, "Max supply exceeded");
 
         _mint(to, id, amount, data);
     }
@@ -56,7 +86,10 @@ contract SoundVerseERC1155 is Context, AccessControlEnumerable, ERC1155Burnable,
         bytes memory data
     ) public virtual {
         _setupRole(MINTER_ROLE, msg.sender);
-        require(hasRole(MINTER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have minter role to mint");
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERC1155PresetMinterPauser: must have minter role to mint"
+        );
 
         _mintBatch(to, ids, amounts, data);
     }
@@ -71,7 +104,10 @@ contract SoundVerseERC1155 is Context, AccessControlEnumerable, ERC1155Burnable,
      * - the caller must have the `PAUSER_ROLE`.
      */
     function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have pauser role to pause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "ERC1155PresetMinterPauser: must have pauser role to pause"
+        );
         _pause();
     }
 
@@ -85,7 +121,10 @@ contract SoundVerseERC1155 is Context, AccessControlEnumerable, ERC1155Burnable,
      * - the caller must have the `PAUSER_ROLE`.
      */
     function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have pauser role to unpause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "ERC1155PresetMinterPauser: must have pauser role to unpause"
+        );
         _unpause();
     }
 
--- test/SoundVerseERC1155.js
+++ test/SoundVerseERC1155.js
@@ -1,6 +1,5 @@
 const { constants } = require("@openzeppelin/test-helpers");
 const { ZERO_ADDRESS } = constants;
-
 const { expect } = require("chai");
 
 describe('SoundVerseERC1155.contract', function () {
@@ -13,41 +12,64 @@ describe('SoundVerseERC1155.contract', function () {
     const secondTokenId = 48324;
     const secondTokenIdAmount = 77875;
 
+    const data = '0x';
+
     const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000';
     const MINTER_ROLE = ethers.utils.solidityKeccak256(['string'], ['MINTER_ROLE']);
     const PAUSER_ROLE = ethers.utils.solidityKeccak256(['string'], ['PAUSER_ROLE']);
 
-    const uri = 'https://gateway.pinata.cloud/ipfs/123.json';
+    const uri = 'https://gateway.pinata.cloud/ipfs/{id}.json';
+    const changedUri = 'https://gateway.pinata.cloud/ipfs/changedUri/{id}.json';
 
     beforeEach(async function () {
         SoundVerseERC1155Factory = await ethers.getContractFactory("SoundVerseERC1155");
         [deployer, other] = await ethers.getSigners();
-        soundVerseERC1155 = await SoundVerseERC1155Factory.deploy(uri);
+        soundVerseERC1155 = await SoundVerseERC1155Factory.deploy();
+
+        await soundVerseERC1155.setTokenUri(48324, uri);
     });
 
-    it('deployer has the default admin role', async function () {
-        expect(await soundVerseERC1155.getRoleMemberCount(DEFAULT_ADMIN_ROLE)).to.equal(1);
-        expect(await soundVerseERC1155.getRoleMember(DEFAULT_ADMIN_ROLE, 0)).to.equal(deployer.address);
+    describe('Initialization', function () {
+
+        it('deployer has the default admin role', async function () {
+            expect(await soundVerseERC1155.getRoleMemberCount(DEFAULT_ADMIN_ROLE)).to.equal(1);
+            expect(await soundVerseERC1155.getRoleMember(DEFAULT_ADMIN_ROLE, 0)).to.equal(deployer.address);
+        });
+
+        it('deployer has the minter role', async function () {
+            expect(await soundVerseERC1155.getRoleMemberCount(MINTER_ROLE)).to.equal(1);
+            expect(await soundVerseERC1155.getRoleMember(MINTER_ROLE, 0)).to.equal(deployer.address);
+        });
+
+        it('deployer has the pauser role', async function () {
+            expect(await soundVerseERC1155.getRoleMemberCount(PAUSER_ROLE)).to.equal(1);
+            expect(await soundVerseERC1155.getRoleMember(PAUSER_ROLE, 0)).to.equal(deployer.address);
+        });
+
+        it('minter and pauser role admin is the default admin', async function () {
+            expect(await soundVerseERC1155.getRoleAdmin(MINTER_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
+            expect(await soundVerseERC1155.getRoleAdmin(PAUSER_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
+        });
+
     });
 
-    it('deployer has the minter role', async function () {
-        expect(await soundVerseERC1155.getRoleMemberCount(MINTER_ROLE)).to.equal(1);
-        expect(await soundVerseERC1155.getRoleMember(MINTER_ROLE, 0)).to.equal(deployer.address);
+    describe('URI', function () {
+        it('Should be able to set uri for token id', async function () {
+            await soundVerseERC1155.setTokenUri(845, uri);
+            expect(await soundVerseERC1155.uri(845)).to.equal(uri);
+        });
+
+        it('Should not be able to set uri for token id with already a given uri', async function () {
+            await soundVerseERC1155.setTokenUri(845, changedUri);
+            await expect(soundVerseERC1155.setTokenUri(845, changedUri))
+                .to.be.revertedWith("Cannot set uri twice");
+        });
+
     });
 
-    it('deployer has the pauser role', async function () {
-        expect(await soundVerseERC1155.getRoleMemberCount(PAUSER_ROLE)).to.equal(1);
-        expect(await soundVerseERC1155.getRoleMember(PAUSER_ROLE, 0)).to.equal(deployer.address);
-    });
-
-    it('minter and pauser role admin is the default admin', async function () {
-        expect(await soundVerseERC1155.getRoleAdmin(MINTER_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
-        expect(await soundVerseERC1155.getRoleAdmin(PAUSER_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
-    });
-
-    describe('minting', function () {
+    describe('Minting', function () {
         it('deployer can mint tokens', async function () {
-            const receipt = await soundVerseERC1155.mint(other.address, firstTokenId, firstTokenIdAmount, '0x', { from: deployer.address });
+            const receipt = await soundVerseERC1155.mint(other.address, firstTokenId, firstTokenIdAmount, data, { from: deployer.address });
             await expect(receipt)
                 .to.emit(soundVerseERC1155, 'TransferSingle')
                 .withArgs(deployer.address, ZERO_ADDRESS, other.address, firstTokenId, firstTokenIdAmount)
@@ -56,7 +78,7 @@ describe('SoundVerseERC1155.contract', function () {
         });
 
         it('other accounts can also mint tokens', async function () {
-            const receipt = await soundVerseERC1155.connect(other).mint(other.address, firstTokenId, firstTokenIdAmount, '0x', { from: other.address });
+            const receipt = await soundVerseERC1155.connect(other).mint(other.address, firstTokenId, firstTokenIdAmount, data, { from: other.address });
             await expect(receipt)
                 .to.emit(soundVerseERC1155, 'TransferSingle')
                 .withArgs(other.address, ZERO_ADDRESS, other.address, firstTokenId, firstTokenIdAmount)
@@ -65,10 +87,10 @@ describe('SoundVerseERC1155.contract', function () {
         });
     });
 
-    describe('batched minting', function () {
+    describe('Batched minting', function () {
         it('deployer can batch mint tokens', async function () {
             const receipt = await soundVerseERC1155.mintBatch(
-                other.address, [firstTokenId, secondTokenId], [firstTokenIdAmount, secondTokenIdAmount], '0x', { from: deployer.address },
+                other.address, [firstTokenId, secondTokenId], [firstTokenIdAmount, secondTokenIdAmount], data, { from: deployer.address },
             );
 
             await expect(receipt)
@@ -80,7 +102,7 @@ describe('SoundVerseERC1155.contract', function () {
 
         it('other accounts can also batch mint tokens', async function () {
             const receipt = await soundVerseERC1155.connect(other).mintBatch(
-                other.address, [firstTokenId, secondTokenId], [firstTokenIdAmount, secondTokenIdAmount], '0x', { from: other.address },
+                other.address, [firstTokenId, secondTokenId], [firstTokenIdAmount, secondTokenIdAmount], data, { from: other.address },
             );
 
             await expect(receipt)
@@ -92,7 +114,7 @@ describe('SoundVerseERC1155.contract', function () {
 
     });
 
-    describe('pausing', function () {
+    describe('Pausing', function () {
         it('deployer can pause', async function () {
             const receipt = await soundVerseERC1155.pause({ from: deployer.address });
             await expect(receipt)
@@ -116,7 +138,7 @@ describe('SoundVerseERC1155.contract', function () {
         it('cannot mint while paused', async function () {
             await soundVerseERC1155.pause({ from: deployer.address });
 
-            await expect(soundVerseERC1155.mint(other.address, firstTokenId, firstTokenIdAmount, '0x', { from: deployer.address })
+            await expect(soundVerseERC1155.mint(other.address, firstTokenId, firstTokenIdAmount, data, { from: deployer.address })
             ).to.be.revertedWith("ERC1155Pausable: token transfer while paused");
         });
 
@@ -131,9 +153,9 @@ describe('SoundVerseERC1155.contract', function () {
         });
     });
 
-    describe('burning', function () {
+    describe('Burning', function () {
         it('holders can burn their tokens', async function () {
-            await soundVerseERC1155.mint(other.address, firstTokenId, firstTokenIdAmount, '0x', { from: deployer.address });
+            await soundVerseERC1155.mint(other.address, firstTokenId, firstTokenIdAmount, data, { from: deployer.address });
 
             const receipt = await soundVerseERC1155.connect(other).burn(other.address, firstTokenId, 4999, { from: other.address });
             expect(receipt)
