e3a96aa82177ea8af58115d6a3e4199ba801a5e2
Camilo Ariza <camiloariza1@icloud.com>
2022-02-16 23:32:55 +0100

    [DEV-279] fix erc721 and erc1155 tests according to lazy minting

--- test/SoundVerseERC1155.js
+++ test/SoundVerseERC1155.js
@@ -2,7 +2,7 @@ const { constants } = require("@openzeppelin/test-helpers");
 const { ZERO_ADDRESS } = constants;
 const { expect } = require("chai");
 
-describe.only('SoundVerseERC1155.contract', function () {
+describe('SoundVerseERC1155.contract', function () {
 
     let soundVerseERC1155;
 
@@ -17,20 +17,20 @@ describe.only('SoundVerseERC1155.contract', function () {
     beforeEach(async function () {
 
         SoundVerseERC1155Factory = await ethers.getContractFactory("SoundVerseERC1155");
-        [deployer, other] = await ethers.getSigners();
+        [signer, buyer] = await ethers.getSigners();
         soundVerseERC1155 = await SoundVerseERC1155Factory.deploy();
     });
 
     describe('Initialization', function () {
 
-        it('deployer has the default admin role', async function () {
+        it('signer has the default admin role', async function () {
             expect(await soundVerseERC1155.getRoleMemberCount(DEFAULT_ADMIN_ROLE)).to.equal(1);
-            expect(await soundVerseERC1155.getRoleMember(DEFAULT_ADMIN_ROLE, 0)).to.equal(deployer.address);
+            expect(await soundVerseERC1155.getRoleMember(DEFAULT_ADMIN_ROLE, 0)).to.equal(signer.address);
         });
 
-        it('deployer has the pauser role', async function () {
+        it('signer has the pauser role', async function () {
             expect(await soundVerseERC1155.getRoleMemberCount(PAUSER_ROLE)).to.equal(1);
-            expect(await soundVerseERC1155.getRoleMember(PAUSER_ROLE, 0)).to.equal(deployer.address);
+            expect(await soundVerseERC1155.getRoleMember(PAUSER_ROLE, 0)).to.equal(signer.address);
         });
 
         it('minter and pauser role admin is the default admin', async function () {
@@ -40,64 +40,57 @@ describe.only('SoundVerseERC1155.contract', function () {
     });
 
     describe('Minting licenses', function () {
-        it('deployer can mint tokens', async function () {
-            const receipt = await soundVerseERC1155.mintLicenses(other.address, uri, firstTokenIdAmount, data, { from: deployer.address });
+        it('signer can mint tokens', async function () {
+            const receipt = await soundVerseERC1155.mintLicenses(signer.address, buyer.address, uri, firstTokenIdAmount, data, { from: signer.address });
 
             expect(await soundVerseERC1155.uri(0)).to.equal(uri);
 
+             // 2. TransferSingle(operator, from, to, id, amount);
             await expect(receipt)
                 .to.emit(soundVerseERC1155, 'TransferSingle')
-                .withArgs(deployer.address, ZERO_ADDRESS, other.address, 0, firstTokenIdAmount)
+                .withArgs(signer.address, signer.address, buyer.address, 0, 1)
 
-            expect(await soundVerseERC1155.balanceOf(other.address, 0)).to.equal(firstTokenIdAmount);
+            expect(await soundVerseERC1155.balanceOf(buyer.address, 0)).to.equal(1);
         });
 
-        it('other accounts can also mint tokens', async function () {
-            const receipt = await soundVerseERC1155.connect(other).mintLicenses(other.address, uri, firstTokenIdAmount, data, { from: other.address });
-            await expect(receipt)
-                .to.emit(soundVerseERC1155, 'TransferSingle')
-                .withArgs(other.address, ZERO_ADDRESS, other.address, 0, firstTokenIdAmount)
-
-            expect(await soundVerseERC1155.connect(other).balanceOf(other.address, 0)).to.equal(firstTokenIdAmount);
-        });
     });
 
     describe('Pausing', function () {
-        it('deployer can pause', async function () {
-            const receipt = await soundVerseERC1155.pause({ from: deployer.address });
+        it('signer can pause', async function () {
+            const receipt = await soundVerseERC1155.pause({ from: signer.address });
             await expect(receipt)
                 .to.emit(soundVerseERC1155, 'Paused')
-                .withArgs(deployer.address);
+                .withArgs(signer.address);
 
             expect(await soundVerseERC1155.paused()).to.equal(true);
         });
 
-        it('deployer can unpause', async function () {
-            await soundVerseERC1155.pause({ from: deployer.address });
+        it('signer can unpause', async function () {
+            await soundVerseERC1155.pause({ from: signer.address });
 
-            const receipt = await soundVerseERC1155.unpause({ from: deployer.address });
+            const receipt = await soundVerseERC1155.unpause({ from: signer.address });
             await expect(receipt)
                 .to.emit(soundVerseERC1155, 'Unpaused')
-                .withArgs(deployer.address);
+                .withArgs(signer.address);
 
             expect(await soundVerseERC1155.paused()).to.equal(false);
         });
 
         it('cannot mint while paused', async function () {
-            await soundVerseERC1155.pause({ from: deployer.address });
+            await soundVerseERC1155.pause({ from: signer.address });
 
-            await expect(soundVerseERC1155.mintLicenses(other.address, uri, firstTokenIdAmount, data, { from: deployer.address })
+            await expect(soundVerseERC1155.mintLicenses(signer.address, buyer.address, uri, firstTokenIdAmount, data, { from: signer.address })
             ).to.be.revertedWith("ERC1155Pausable: token transfer while paused");
         });
 
-        it('other accounts cannot pause', async function () {
-            await expect(soundVerseERC1155.connect(other).pause()).to.be.revertedWith("Must have pauser role to pause");
+        it('buyer accounts cannot pause', async function () {
+            await expect(soundVerseERC1155.connect(buyer).pause()).to.be.revertedWith("Must have pauser role to pause");
         });
 
-        it('other accounts cannot unpause', async function () {
-            await soundVerseERC1155.pause({ from: deployer.address });
+        it('buyer accounts cannot unpause', async function () {
+            await soundVerseERC1155.pause({ from: signer.address });
 
-            await expect(soundVerseERC1155.connect(other).unpause({ from: other.address })).to.be.revertedWith("Must have pauser role to unpause");
+            await expect(soundVerseERC1155.connect(buyer).unpause({ from: buyer.address })).to.be.revertedWith("Must have pauser role to unpause");
         });
     });
 
--- test/SoundVerseERC721.js
+++ test/SoundVerseERC721.js
@@ -1,4 +1,3 @@
-const { constants } = require("@openzeppelin/test-helpers");
 const { expect } = require("chai");
 
 describe('NFT contract', function () {
@@ -14,24 +13,27 @@ describe('NFT contract', function () {
         commonUtils = await CommonUtils.deploy()
 
         SoundVerseERC1155 = await ethers.getContractFactory('SoundVerseERC1155');
-        [owner, addr1] = await ethers.getSigners();
+        [owner, addr1, operator] = await ethers.getSigners();
         soundVerseERC1155 = await SoundVerseERC1155.deploy();
 
         await commonUtils.setContractAddressFor('SoundVerseERC1155', soundVerseERC1155.address);
-
+        
         SoundVerseERC721 = await ethers.getContractFactory('SoundVerseERC721');
-        [owner, addr1] = await ethers.getSigners();
+        [owner, addr1, operator] = await ethers.getSigners();
         soundVerseERC721 = await SoundVerseERC721.deploy(commonUtils.address);
 
     });
 
     it('creates 1 Master Nft and its licenses', async function () {
 
-        await expect(soundVerseERC721.createMasterItem(addr1.address, tokenURIOne, 2))
+        console.log("addr1.address",addr1.address);
+        console.log("owner.address",owner.address);
+
+        await expect(soundVerseERC721.createMasterItem(addr1.address, owner.address, tokenURIOne, 2))
             .to.emit(soundVerseERC721, 'MasterMintEvent')
             .withArgs(0);
 
-        await expect(soundVerseERC721.createMasterItem(addr1.address, tokenURITwo, 3))
+        await expect(soundVerseERC721.createMasterItem(addr1.address, owner.address, tokenURITwo, 3))
             .to.emit(soundVerseERC721, 'MasterMintEvent')
             .withArgs(1);
 
@@ -39,25 +41,25 @@ describe('NFT contract', function () {
 
     it('should revert transaction if no tokenUri present', async function () {
 
-        await expect(soundVerseERC721.createMasterItem(addr1.address, "", 2))
+        await expect(soundVerseERC721.createMasterItem(addr1.address, owner.address, "", 2))
             .to.be.revertedWith("TokenUri can not be null");
 
     });
 
     it('should revert transaction if no number of licenses is less than 2', async function () {
 
-        await expect(soundVerseERC721.createMasterItem(addr1.address, tokenURIOne, 1))
+        await expect(soundVerseERC721.createMasterItem(addr1.address, owner.address, tokenURIOne, 1))
             .to.be.revertedWith("Supply must be greater than 2");
 
     });
 
     it('should call erc1155 contract', async function () {
-        // const tokenId = await commonUtils.toBytes(1);
+        const masterId = await commonUtils.toBytes(1);
 
-        // await expect(soundVerseERC1155.mintLicenses(addr1.address, tokenURIOne, 2, tokenId))
-        //     .to.be
-
-        expect('mintLicenses').to.be.calledOnContract(soundVerseERC1155);
+        // TransferSingle(operator, from, to, id, amount);
+        await expect(soundVerseERC1155.connect(addr1).mintLicenses(owner.address, addr1.address, tokenURIOne, 2, masterId))
+            .to.emit(soundVerseERC1155, 'TransferSingle')
+            .withArgs(addr1.address, owner.address, addr1.address, 0, 1);
 
     });
 
