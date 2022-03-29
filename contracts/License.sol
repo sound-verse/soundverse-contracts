ad2ff82d9eab347b22b7c9a521dad216c430284a
Camilo Ariza <camiloariza1@icloud.com>
2022-03-29 22:16:40 +0200

    add transfer master implementation

--- contracts/License.sol
+++ contracts/License.sol
@@ -12,182 +12,197 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract License is
-    Context,
-    AccessControlEnumerable,
-    ERC1155Burnable,
-    ERC1155Pausable,
-    Ownable
+  Context,
+  AccessControlEnumerable,
+  ERC1155Burnable,
+  ERC1155Pausable,
+  Ownable
 {
-    using Counters for Counters.Counter;
+  using Counters for Counters.Counter;
 
-    //Constants and variables
-    Counters.Counter internal _licenseBundleId;
-    bytes32 internal constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    ICommonUtils internal commonUtils;
+  //Constants and variables
+  Counters.Counter internal _licenseBundleId;
+  bytes32 internal constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+  ICommonUtils internal commonUtils;
 
-    mapping(uint256 => string) public _uris;
+  mapping(uint256 => string) public _uris;
 
-    // Events
-    event LicenseMintEvent(uint256 indexed id, string uri);
+  // Events
+  event LicenseMintEvent(uint256 indexed id, string uri);
 
-    /**
-     * @dev Grants `DEFAULT_ADMIN_ROLE` and `PAUSER_ROLE` to the account that
-     * deploys the contract.
-     */
-    constructor(address _commonUtilsAddress) ERC1155("") {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _setupRole(PAUSER_ROLE, _msgSender());
-        commonUtils = ICommonUtils(_commonUtilsAddress);
-    }
+  /**
+   * @dev Grants `DEFAULT_ADMIN_ROLE` and `PAUSER_ROLE` to the account that
+   * deploys the contract.
+   */
+  constructor(address _commonUtilsAddress) ERC1155("") {
+    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    _setupRole(PAUSER_ROLE, _msgSender());
+    commonUtils = ICommonUtils(_commonUtilsAddress);
+  }
 
-    /**
-     * @dev Returns the actual `baseURI` for token type `id`.
-     * @param tokenId token ID to retrieve the uri from
-     * @return URI from token ID
-     */
-    function uri(uint256 tokenId) public view override returns (string memory) {
-        return (_uris[tokenId]);
-    }
+  /**
+   * @dev Returns the actual `baseURI` for token type `id`.
+   * @param tokenId token ID to retrieve the uri from
+   * @return URI from token ID
+   */
+  function uri(uint256 tokenId) public view override returns (string memory) {
+    return (_uris[tokenId]);
+  }
 
-    /**
-     * @dev Sets token URI for token type `id`.
-     * @param tokenId token ID receiving the uri
-     * @param _uri URI to set to token ID
-     */
-    function setTokenUri(uint256 tokenId, string memory _uri) public {
-        require(bytes(_uris[tokenId]).length == 0, "Cannot set uri twice");
-        _uris[tokenId] = _uri;
-    }
+  /**
+   * @dev Sets token URI for token type `id`.
+   * @param tokenId token ID receiving the uri
+   * @param _uri URI to set to token ID
+   */
+  function setTokenUri(uint256 tokenId, string memory _uri) public {
+    require(bytes(_uris[tokenId]).length == 0, "Cannot set uri twice");
+    _uris[tokenId] = _uri;
+  }
 
-    /**
-     * @dev Creates `amount` new tokens for `to`, of token type `id`.
-     * See {ERC1155-_mint}.
-     * @param _signer address of the creator
-     * @param _mintURI URI of the song
-     * @param _amount amount of licenses to be created
-     * @param _erc721Reference reference of the Master NFT
-     */
-    function mintLicenses(
-        address _signer,
-        string memory _mintURI,
-        uint256 _amount,
-        bytes memory _erc721Reference
-    ) public onlyMaster {
-        _licenseBundleId.increment();
-        uint256 currentLicenseBundleId = _licenseBundleId.current();
-        
-        mint(
-            _signer,
-            currentLicenseBundleId,
-            _mintURI,
-            _amount,
-            _erc721Reference
-        );
-    }
+  /**
+   * @dev Creates `amount` new tokens for `to`, of token type `id`.
+   * See {ERC1155-_mint}.
+   * @param _signer address of the creator
+   * @param _mintURI URI of the song
+   * @param _amount amount of licenses to be created
+   * @param _erc721Reference reference of the Master NFT
+   */
+  function mintLicenses(
+    address _signer,
+    string memory _mintURI,
+    uint256 _amount,
+    bytes memory _erc721Reference
+  ) public onlyMaster {
+    _licenseBundleId.increment();
+    uint256 currentLicenseBundleId = _licenseBundleId.current();
 
-    /**
-     * @dev Mint Master
-     * @param _signer address of the creator
-     * @param _currentLicenseBundleId ID of the Master NFT
-     * @param _mintURI URI of the song
-     * @param _amount amount of licenses to be created
-     * @param _erc721Reference reference of the Master NFT
-     */
-    function mint(
-        address _signer,
-        uint256 _currentLicenseBundleId,
-        string memory _mintURI,
-        uint256 _amount,
-        bytes memory _erc721Reference
-    ) private {
-        setTokenUri(_currentLicenseBundleId, _mintURI);
-        _mint(_signer, _currentLicenseBundleId, _amount, _erc721Reference);
+    mint(_signer, currentLicenseBundleId, _mintURI, _amount, _erc721Reference);
+  }
 
-        emit LicenseMintEvent(_currentLicenseBundleId, _mintURI);
-    }
+  /**
+   * @dev Mint Master
+   * @param _signer address of the creator
+   * @param _currentLicenseBundleId ID of the Master NFT
+   * @param _mintURI URI of the song
+   * @param _amount amount of licenses to be created
+   * @param _erc721Reference reference of the Master NFT
+   */
+  function mint(
+    address _signer,
+    uint256 _currentLicenseBundleId,
+    string memory _mintURI,
+    uint256 _amount,
+    bytes memory _erc721Reference
+  ) private {
+    setTokenUri(_currentLicenseBundleId, _mintURI);
+    _mint(_signer, _currentLicenseBundleId, _amount, _erc721Reference);
 
-    /**
-     * @dev Keeps track of total minted nfts
-     * @return total of minted license bundles
-     */
-    function totalTokens() public view returns (uint256) {
-        return _licenseBundleId.current();
-    }
+    emit LicenseMintEvent(_currentLicenseBundleId, _mintURI);
+  }
 
-    /**
-     * @dev Pause
-     *
-     * See {ERC1155Pausable} and {Pausable-_pause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function pause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "Must have pauser role to pause"
-        );
-        _pause();
-    }
+  /**
+   * @dev Keeps track of total minted nfts
+   * @return total of minted license bundles
+   */
+  function totalTokens() public view returns (uint256) {
+    return _licenseBundleId.current();
+  }
 
-    /**
-     * @dev Unpauses all token transfers.
-     *
-     * See {ERC1155Pausable} and {Pausable-_unpause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function unpause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "Must have pauser role to unpause"
-        );
-        _unpause();
-    }
+  /**
+   * @dev Pause
+   *
+   * See {ERC1155Pausable} and {Pausable-_pause}.
+   *
+   * Requirements:
+   *
+   * - the caller must have the `PAUSER_ROLE`.
+   */
+  function pause() public virtual {
+    require(
+      hasRole(PAUSER_ROLE, _msgSender()),
+      "Must have pauser role to pause"
+    );
+    _pause();
+  }
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(AccessControlEnumerable, ERC1155)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  /**
+   * @dev Unpauses all token transfers.
+   *
+   * See {ERC1155Pausable} and {Pausable-_unpause}.
+   *
+   * Requirements:
+   *
+   * - the caller must have the `PAUSER_ROLE`.
+   */
+  function unpause() public virtual {
+    require(
+      hasRole(PAUSER_ROLE, _msgSender()),
+      "Must have pauser role to unpause"
+    );
+    _unpause();
+  }
 
-    function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal virtual override(ERC1155, ERC1155Pausable) {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-    }
+  /**
+   * @dev See {IERC165-supportsInterface}.
+   */
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    virtual
+    override(AccessControlEnumerable, ERC1155)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
 
-    modifier onlyMaster() {
-        require(msg.sender == commonUtils.getContractAddressFrom("Master"));
-        _;
-    }
+  function _beforeTokenTransfer(
+    address operator,
+    address from,
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) internal virtual override(ERC1155, ERC1155Pausable) {
+    super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+  }
 
-    function transferLicenses(
-        address _signer,
-        address _buyer,
-        uint256 _currentLicenseBundleId,
-        uint256 _amountToPurchase
-    ) public {
-        _safeTransferFrom(_signer, _buyer, _currentLicenseBundleId, _amountToPurchase, "0x");
-    }
+  modifier onlyMaster() {
+    require(msg.sender == commonUtils.getContractAddressFrom("Master"));
+    _;
+  }
 
-    function licensesBalanceOf(address account, uint256 id) public view returns(uint256){
-        return balanceOf(account, id);
-    }
+  /**
+   * @dev Transfer license
+   * @param _signer From address
+   * @param _buyer To address
+   * @param _currentLicenseBundleId ID of the token bundle to be transferred
+   * @param _amountToPurchase Amount to be transferred
+   */
+  function transferLicenses(
+    address _signer,
+    address _buyer,
+    uint256 _currentLicenseBundleId,
+    uint256 _amountToPurchase
+  ) public {
+    _safeTransferFrom(
+      _signer,
+      _buyer,
+      _currentLicenseBundleId,
+      _amountToPurchase,
+      "0x"
+    );
+  }
 
+  /**
+   * @dev License balance of
+   * @param _account From address
+   * @param _tokenId Token ID to search for
+   */
+  function licensesBalanceOf(address _account, uint256 _tokenId)
+    public
+    view
+    returns (uint256)
+  {
+    return balanceOf(_account, _tokenId);
+  }
 }
--- contracts/Master.sol
+++ contracts/Master.sol
@@ -15,230 +15,239 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "hardhat/console.sol";
 
 contract Master is
-    AccessControlEnumerable,
-    ERC721URIStorage,
-    Pausable,
-    Ownable
+  AccessControlEnumerable,
+  ERC721URIStorage,
+  Pausable,
+  Ownable
 {
-    using SafeMath for uint256;
-    using Counters for Counters.Counter;
+  using SafeMath for uint256;
+  using Counters for Counters.Counter;
 
-    // Contracts and libraries
-    ICommonUtils internal commonUtils;
-    ILicense internal licensesContract;
+  // Contracts and libraries
+  ICommonUtils internal commonUtils;
+  ILicense internal licensesContract;
 
-    // Constants and variables
-    bytes32 internal constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    string internal constant LICENSE = "License";
-    uint256 internal constant MIN_SUPPLY = 2;
-    Counters.Counter private _tokenIdTracker;
+  // Constants and variables
+  bytes32 internal constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+  string internal constant LICENSE = "License";
+  uint256 internal constant MIN_SUPPLY = 2;
+  Counters.Counter private _tokenIdTracker;
 
-    // Events
-    event MasterMintEvent(uint256 indexed id, string uri);
+  // Events
+  event MasterMintEvent(uint256 indexed id, string uri);
 
-    /**
-     * @dev Constructor of Master NFT
-     * @param _commonUtilsAddress CommonUtils contract address
-     */
-    constructor(address _commonUtilsAddress) ERC721("SoundVerseMaster", "SVM") {
-        _setupRole(PAUSER_ROLE, _msgSender());
+  /**
+   * @dev Constructor of Master NFT
+   * @param _commonUtilsAddress CommonUtils contract address
+   */
+  constructor(address _commonUtilsAddress) ERC721("SoundVerseMaster", "SVM") {
+    _setupRole(PAUSER_ROLE, _msgSender());
 
-        commonUtils = ICommonUtils(_commonUtilsAddress);
-        address licenseAddress = commonUtils.getContractAddressFrom(LICENSE);
-        licensesContract = ILicense(licenseAddress);
-    }
+    commonUtils = ICommonUtils(_commonUtilsAddress);
+    address licenseAddress = commonUtils.getContractAddressFrom(LICENSE);
+    licensesContract = ILicense(licenseAddress);
+  }
 
-    /**
-     * @dev Project creator mapping
-     */
-    mapping(uint256 => address) public _creators;
+  /**
+   * @dev Project creator mapping
+   */
+  mapping(uint256 => address) public _creators;
 
-    /**
-     * @dev Sets token creator for token type `id`.
-     * @param tokenId token ID receiving the creator
-     * @param _creator Creator of the project
-     */
-    function setTokenCreator(uint256 tokenId, address _creator) internal {
-        _creators[tokenId] = _creator;
-    }
+  /**
+   * @dev Sets token creator for token type `id`.
+   * @param tokenId token ID receiving the creator
+   * @param _creator Creator of the project
+   */
+  function setTokenCreator(uint256 tokenId, address _creator) internal {
+    _creators[tokenId] = _creator;
+  }
 
-    /**
-     * @param tokenId token ID receiving the creator
-     * @return address from creator of the project
-     */
-    function creator(uint256 tokenId) internal view returns (address) {
-        return _creators[tokenId];
-    }
+  /**
+   * @param tokenId token ID receiving the creator
+   * @return address from creator of the project
+   */
+  function creator(uint256 tokenId) internal view returns (address) {
+    return _creators[tokenId];
+  }
 
-    /**
-     * @dev tokenURIs
-     */
-    mapping(uint256 => string) public _uris;
+  /**
+   * @dev tokenURIs
+   */
+  mapping(uint256 => string) public _uris;
 
-    /**
-     * @dev TokenIDs from URIs
-     */
-    mapping(string => uint256) public _urisToIds;
+  /**
+   * @dev TokenIDs from URIs
+   */
+  mapping(string => uint256) public _urisToIds;
 
-    /**
-     * @dev Returns the actual URI for TokenID.
-     * @param tokenId token ID to retrieve the uri from
-     * @return URI from token ID
-     */
-    function uri(uint256 tokenId) public view returns (string memory) {
-        return (_uris[tokenId]);
-    }
+  /**
+   * @dev Returns the actual URI for TokenID.
+   * @param tokenId token ID to retrieve the uri from
+   * @return URI from token ID
+   */
+  function uri(uint256 tokenId) public view returns (string memory) {
+    return (_uris[tokenId]);
+  }
 
-    /**
-     * @dev Returns the actual TokenID for a given URI
-     * @param _uri URI to retrieve the TokenID from
-     * @return URI from token ID
-     */
-    function tokenIdForURI(string memory _uri) public view returns (uint256) {
-        return (_urisToIds[_uri]);
-    }
+  /**
+   * @dev Returns the actual TokenID for a given URI
+   * @param _uri URI to retrieve the TokenID from
+   * @return URI from token ID
+   */
+  function tokenIdForURI(string memory _uri) public view returns (uint256) {
+    return (_urisToIds[_uri]);
+  }
 
-    /**
-     * @dev Sets tokenID for token URI.
-     * @param _tokenId token ID for a given URI
-     * @param _uri URI of NFT
-     */
-    function setTokenIDForURI(uint256 _tokenId, string memory _uri) internal {
-        _urisToIds[_uri] = _tokenId;
-    }
+  /**
+   * @dev Sets tokenID for token URI.
+   * @param _tokenId token ID for a given URI
+   * @param _uri URI of NFT
+   */
+  function setTokenIDForURI(uint256 _tokenId, string memory _uri) internal {
+    _urisToIds[_uri] = _tokenId;
+  }
 
-    /**
-     * @dev Main minting function
-     * Function to called by the BE to trigger Master and License minting
-     * @param _signer address of creator
-     * @param tokenURI URI of the song to be minted
-     * @param _licensesAmount amount of licenses to mint linked to the Master NFT
-     * Finally approves the Marketplace to handle the Master
-     */
-    function createMasterItem(
-        address _signer,
-        string memory tokenURI,
-        uint256 _licensesAmount
-    ) public onlyMarketplace returns (uint256) {
-        require(bytes(tokenURI).length > 0, "TokenUri can not be null");
-        require(_licensesAmount >= MIN_SUPPLY, "Supply must be greater than 2");
-        uint256 tokenId = mintItem(_signer, tokenURI, _licensesAmount);
+  /**
+   * @dev Main minting function
+   * Function to called by the BE to trigger Master and License minting
+   * @param _signer address of creator
+   * @param tokenURI URI of the song to be minted
+   * @param _licensesAmount amount of licenses to mint linked to the Master NFT
+   * Finally approves the Marketplace to handle the Master
+   */
+  function createMasterItem(
+    address _signer,
+    string memory tokenURI,
+    uint256 _licensesAmount
+  ) public onlyMarketplace returns (uint256) {
+    require(bytes(tokenURI).length > 0, "TokenUri can not be null");
+    require(_licensesAmount >= MIN_SUPPLY, "Supply must be greater than 2");
+    uint256 tokenId = mintItem(_signer, tokenURI, _licensesAmount);
 
-        return tokenId;
-    }
+    return tokenId;
+  }
 
-    /**
-     * @dev Mint function that mints Master NFT and linked licenses
-     * @param _signer address of creator
-     * @param _mintURI URI of the song to be minted
-     * @param _amountToMint amount of licenses to mint linked to the Master NFT
-     */
-    function mintItem(
-        address _signer,
-        string memory _mintURI,
-        uint256 _amountToMint
-    ) private returns (uint256) {
-        _tokenIdTracker.increment();
-        uint256 currentTokenId = _tokenIdTracker.current();
+  /**
+   * @dev Mint function that mints Master NFT and linked licenses
+   * @param _signer address of creator
+   * @param _mintURI URI of the song to be minted
+   * @param _amountToMint amount of licenses to mint linked to the Master NFT
+   */
+  function mintItem(
+    address _signer,
+    string memory _mintURI,
+    uint256 _amountToMint
+  ) private returns (uint256) {
+    _tokenIdTracker.increment();
+    uint256 currentTokenId = _tokenIdTracker.current();
 
-        require(
-            bytes(_uris[currentTokenId]).length == 0,
-            "Cannot set URI twice"
-        );
+    require(bytes(_uris[currentTokenId]).length == 0, "Cannot set URI twice");
 
-        // Creator in mapping speichern anhand tokenId
-        setTokenCreator(currentTokenId, _signer);
+    // Creator in mapping speichern anhand tokenId
+    setTokenCreator(currentTokenId, _signer);
 
-        _safeMint(_signer, currentTokenId);
-        _setTokenURI(currentTokenId, _mintURI);
-        setTokenIDForURI(currentTokenId, _mintURI);
-        emit MasterMintEvent(currentTokenId, _mintURI);
+    _safeMint(_signer, currentTokenId);
+    _setTokenURI(currentTokenId, _mintURI);
+    setTokenIDForURI(currentTokenId, _mintURI);
+    emit MasterMintEvent(currentTokenId, _mintURI);
 
-        licensesContract.mintLicenses(
-            _signer,
-            _mintURI,
-            _amountToMint,
-            commonUtils.toBytes(currentTokenId)
-        );
-        console.log("CURRENT TOKEN ID =====", currentTokenId);
-        uint256 tokId = tokenIdForURI(_mintURI);
-        console.log("TOKEN ID FOR URI======", tokId);
+    licensesContract.mintLicenses(
+      _signer,
+      _mintURI,
+      _amountToMint,
+      commonUtils.toBytes(currentTokenId)
+    );
+    console.log("CURRENT TOKEN ID =====", currentTokenId);
+    uint256 tokId = tokenIdForURI(_mintURI);
+    console.log("TOKEN ID FOR URI======", tokId);
 
-        return currentTokenId;
-    }
+    return currentTokenId;
+  }
 
-    /**
-     * @dev Burn tokens
-     * @param tokenId ID of the token to be burned
-     */
-    function burnUnpublishedItem(uint256 tokenId) public onlyOwner {
-        _burn(tokenId);
-    }
+  /**
+   * @dev Transfer Master
+   * @param _signer From address
+   * @param _buyer To address
+   * @param _currentTokenId ID of the token to be transferred
+   */
+  function transferMaster(
+    address _signer,
+    address _buyer,
+    uint256 _currentTokenId
+  ) public {
+    _safeTransfer(_signer, _buyer, _currentTokenId, "0x");
+  }
 
-    /**
-     * @dev Keeps track of total minted nfts
-     * @return current number of minted tokens
-     */
-    function totalTokens() public view returns (uint256) {
-        return _tokenIdTracker.current();
-    }
+  /**
+   * @dev Burn tokens
+   * @param tokenId ID of the token to be burned
+   */
+  function burnUnpublishedItem(uint256 tokenId) public onlyOwner {
+    _burn(tokenId);
+  }
 
-    /**
-     * @dev Pause
-     * See {ERC1155Pausable} and {Pausable-_pause}.
-     * Requirements:
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function pause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "Must have pauser role to pause"
-        );
-        _pause();
-    }
+  /**
+   * @dev Keeps track of total minted nfts
+   * @return current number of minted tokens
+   */
+  function totalTokens() public view returns (uint256) {
+    return _tokenIdTracker.current();
+  }
 
-    /**
-     * @dev Unpauses all token transfers.
-     * See {ERC1155Pausable} and {Pausable-_unpause}.
-     * Requirements:
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function unpause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "Must have pauser role to unpause"
-        );
-        _unpause();
-    }
+  /**
+   * @dev Pause
+   * See {ERC1155Pausable} and {Pausable-_pause}.
+   * Requirements:
+   * - the caller must have the `PAUSER_ROLE`.
+   */
+  function pause() public virtual {
+    require(
+      hasRole(PAUSER_ROLE, _msgSender()),
+      "Must have pauser role to pause"
+    );
+    _pause();
+  }
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(AccessControlEnumerable, ERC721)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  /**
+   * @dev Unpauses all token transfers.
+   * See {ERC1155Pausable} and {Pausable-_unpause}.
+   * Requirements:
+   * - the caller must have the `PAUSER_ROLE`.
+   */
+  function unpause() public virtual {
+    require(
+      hasRole(PAUSER_ROLE, _msgSender()),
+      "Must have pauser role to unpause"
+    );
+    _unpause();
+  }
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override(ERC721) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+  /**
+   * @dev See {IERC165-supportsInterface}.
+   */
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    virtual
+    override(AccessControlEnumerable, ERC721)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
 
-    modifier onlyMarketplace() {
-        require(
-            msg.sender == commonUtils.getContractAddressFrom("MarketContract")
-        );
-        _;
-    }
+  /**
+   * @dev See {IERC165-supportsInterface}.
+   */
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal virtual override(ERC721) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  modifier onlyMarketplace() {
+    require(msg.sender == commonUtils.getContractAddressFrom("MarketContract"));
+    _;
+  }
 }
